0f21f69a3ba05357bf586681bb450c25e94d9a7c
manuelsanchez2 <manusansan22@gmail.com>
2020-11-02 12:31:00 +0100

    Add all the keyboard products

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalStyle } from "./GlobalStyles";
 import Hero from "./components/Hero";
 import Products from "./components/Products";
-import { productData } from "./components/Products/data";
+import {
+  productDataComputers,
+  productDataKeyboards,
+  productDataMice,
+} from "./components/Products/data";
 import Feature from "./components/FeatureProduct";
 
 function App() {
@@ -11,8 +15,13 @@ function App() {
     <Router>
       <GlobalStyle />
       <Hero />
-      <Products heading="Choose a friend you can rely on" data={productData} />
+      <Products
+        heading="Choose a friend you can rely on"
+        data={productDataComputers}
+      />
       <Feature />
+      <Products heading="How fast can you type?" data={productDataKeyboards} />
+      <Products heading="Choose the perfect Mouse" data={productDataMice} />
     </Router>
   );
 }
--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -2,7 +2,71 @@ import computer1 from "../../images/computer1.jpg";
 import computer2 from "../../images/computer2.jpg";
 import computer3 from "../../images/computer3.jpg";
 
-export const productData = [
+import keyboard1 from "../../images/keyboard1.jpg";
+import keyboard2 from "../../images/keyboard2.jpg";
+import keyboard3 from "../../images/keyboard3.png";
+
+export const productDataComputers = [
+  {
+    img: computer1,
+    alt: "Lenovo Thinkpad",
+    name: "Lenovo Thinkpad",
+    desc:
+      '2nd Generation 11.6" Chromebook that you are going to love. Great for starters with a moderate budget',
+    price: "$379.99",
+    button: "Add to cart",
+  },
+  {
+    img: computer2,
+    alt: "Dell XPS 13",
+    name: "Dell XPS 13",
+    desc:
+      "Power and size join together in this versatile computer. Check it out its powerful 10th Gen Intel Core processor!",
+    price: "$779.99",
+    button: "Add to cart",
+  },
+  {
+    img: computer3,
+    alt: "MacBook Pro",
+    name: "MacBook Pro 15",
+    desc:
+      "Computer designed to bring your work to the very next level. Did we mention it has a beautiful Retina display?",
+    price: "$1299.99",
+    button: "Add to cart",
+  },
+];
+
+export const productDataKeyboards = [
+  {
+    img: keyboard1,
+    alt: "Omoton Keyboard",
+    name: "Omoton Keyboard",
+    desc:
+      "Best solution for beginners! Really cheap and it helps you improve your coding skills! It is in offer now!",
+    price: "$11.99",
+    button: "Add to cart",
+  },
+  {
+    img: keyboard2,
+    alt: "Microsoft Ergonomic Keyboard",
+    name: "Ergonomic Keyboard",
+    desc:
+      "Microsoft Ergonomic Keyboard offers an unique design and functionalities to improve your coding experience!",
+    price: "$88.99",
+    button: "Add to cart",
+  },
+  {
+    img: keyboard3,
+    alt: "LogiTech G PRO TKL",
+    name: "LogiTech G PRO TKL",
+    desc:
+      "The best auto-present you can do at Christmas this year! It is done from professionals for professionals!",
+    price: "$132.99",
+    button: "Add to cart",
+  },
+];
+
+export const productDataMice = [
   {
     img: computer1,
     alt: "Lenovo Thinkpad",
