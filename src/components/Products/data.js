6ff4170ac75a7c17072b113d6a456686dc8fae74
manuelsanchez2 <manusansan22@gmail.com>
2020-11-02 12:44:18 +0100

    Add info on the mouse products

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -6,6 +6,10 @@ import keyboard1 from "../../images/keyboard1.jpg";
 import keyboard2 from "../../images/keyboard2.jpg";
 import keyboard3 from "../../images/keyboard3.png";
 
+import mouse1 from "../../images/mouse1.jpg";
+import mouse2 from "../../images/mouse2.jpg";
+import mouse3 from "../../images/mouse3.jpg";
+
 export const productDataComputers = [
   {
     img: computer1,
@@ -68,30 +72,30 @@ export const productDataKeyboards = [
 
 export const productDataMice = [
   {
-    img: computer1,
-    alt: "Lenovo Thinkpad",
-    name: "Lenovo Thinkpad",
+    img: mouse1,
+    alt: "Microsoft Basic Mouse",
+    name: "Microsoft Basic Mouse",
     desc:
-      '2nd Generation 11.6" Chromebook that you are going to love. Great for starters with a moderate budget',
-    price: "$379.99",
+      "Basic mouse created by Microsoft for business people. Cool black colour!",
+    price: "$16.99",
     button: "Add to cart",
   },
   {
-    img: computer2,
-    alt: "Dell XPS 13",
-    name: "Dell XPS 13",
+    img: mouse2,
+    alt: "Magic Mouse 2",
+    name: "Apple Magic Mouse n-2",
     desc:
-      "Power and size join together in this versatile computer. Check it out its powerful 10th Gen Intel Core processor!",
-    price: "$779.99",
+      "A must if you are using Macos environment! Available in two different colors!",
+    price: "$72.99",
     button: "Add to cart",
   },
   {
-    img: computer3,
-    alt: "MacBook Pro",
-    name: "MacBook Pro 15",
+    img: mouse3,
+    alt: "Logitech Mouse",
+    name: "Logitech Casual Mouse",
     desc:
-      "Computer designed to bring your work to the very next level. Did we mention it has a beautiful Retina display?",
-    price: "$1299.99",
+      "Do you also play games? Then do not overthink and choose this one! You will never regret it!",
+    price: "$36.99",
     button: "Add to cart",
   },
 ];
