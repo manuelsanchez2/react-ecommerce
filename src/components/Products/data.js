5c7d599c5bf99c937a87b97b8d5c7ea58b2096c1
manuelsanchez2 <manusansan22@gmail.com>
2020-11-02 11:15:27 +0100

    Create structure of the main Products file

---
+++ src/components/Products/data.js
@@ -0,0 +1,33 @@
+import computer1 from "../../images/computer1.jpg";
+import computer2 from "../../images/computer2.jpg";
+import computer3 from "../../images/computer3.jpg";
+
+export const productData = [
+  {
+    img: computer1,
+    alt: "Lenovo Thinkpad",
+    name: "Lenovo Thinkpad",
+    desc:
+      '2nd Generation 11.6" Chromebook that you are going to love. Great for starters with a moderate budget',
+    price: "$379.99",
+    button: "Add to cart",
+  },
+  {
+    img: computer2,
+    alt: "Dell XPS 13",
+    name: "Dell XPS 13",
+    desc:
+      "Power and size join together in this versatile computer. Check it out its powerful 10th Gen Intel Core processor!",
+    price: "$779.99",
+    button: "Add to cart",
+  },
+  {
+    img: computer3,
+    alt: "MacBook Pro",
+    name: "MacBook Pro 15",
+    desc:
+      "Computer designed to bring your work to the very next level. Did we mention it has a beautiful Retina display?",
+    price: "$1299.99",
+    button: "Add to cart",
+  },
+];
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,10 +1,36 @@
 import React from "react";
+import {
+  ProductsContainer,
+  ProductsHeading,
+  ProductsWrapper,
+  ProductCard,
+  ProductImg,
+  ProductInfo,
+  ProductTitle,
+  ProductDesc,
+  ProductPrice,
+  ProductButton,
+} from "./ProductsElements";
 
 const Products = () => {
   return (
     <ProductsContainer>
       <ProductsHeading>Heading</ProductsHeading>
-      <ProductsWrapper></ProductsWrapper>
+      <ProductsWrapper>
+        {data.map((product, index) => {
+          return (
+            <ProductCard key={index}>
+              <ProductImg src={product.img} alt={product.alt} />
+              <ProductInfo>
+                <ProductTitle>{product.name}</ProductTitle>
+                <ProductDesc>{product.desc}</ProductDesc>
+                <ProductPrice>{product.price}</ProductPrice>
+                <ProductButton>{product.button}</ProductButton>
+              </ProductInfo>
+            </ProductCard>
+          );
+        })}
+      </ProductsWrapper>
     </ProductsContainer>
   );
 };
